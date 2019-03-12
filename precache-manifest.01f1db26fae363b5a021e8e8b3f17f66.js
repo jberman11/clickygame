acbd30a59c9c5824c3ae4bd625aec11c67b9d568
jberman <jarretberman@gmail.com>
2019-03-11 21:57:17 -0500

    Updates

---
+++ precache-manifest.01f1db26fae363b5a021e8e8b3f17f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "976821e7c7c83f0311bc",
+    "url": "/clickygame/static/js/runtime~main.976821e7.js"
+  },
+  {
+    "revision": "36283fce310fa68df65f",
+    "url": "/clickygame/static/js/main.36283fce.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/clickygame/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "36283fce310fa68df65f",
+    "url": "/clickygame/static/css/main.cb81db45.chunk.css"
+  },
+  {
+    "revision": "38008cfc2126c71df7dc7130eef3c375",
+    "url": "/clickygame/index.html"
+  }
+];
